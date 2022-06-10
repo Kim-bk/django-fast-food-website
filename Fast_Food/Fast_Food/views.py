423a325eb23de221dc643726a23486a594040c31
Tien Chung <tienchungcongan@gmail.com>
2022-06-10 22:47:54 +0700

    add_login

--- Fast_Food/Fast_Food/views.py
+++ Fast_Food/Fast_Food/views.py
@@ -2,6 +2,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(request):
+def combonhom_view(request):
     return render(request, "ComboNhom.html",{})
+
+def combomotnguoi_view(request):
+    return render(request, "Combo1Nguoi.html",{})
+
+def MenuUuDai_view(request):
+    return render(request, "MenuUuDai.html",{})
 
--- Fast_Food/accounts/views.py
+++ Fast_Food/accounts/views.py
@@ -1,20 +1,34 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from flask import redirect
 # Create your views here.
+from .form import CreateUserForm
+
 def login_view(request):
     if request.method =="POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(request,username=username,
                                     password=password)
-        if user is None:
-            context = {"Sai tài khoản hoặc mật khẩu"}
-            return render(request, "login.html",context)
+        if user is not None:
+            login(request,user)
+            return render(request, "login.html",{})
     return render(request, "login.html",{})
 
 def logout_view(request):
     return render(request, "logout.html",{})
 
 def register_view(request):
-    return render(request, 'register.html',{})
+    form = CreateUserForm()
+    if request.method =="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Tài khoản '+ user +' đăng ký thành công !')
+    
+    context = {'form':form}
+    return render(request, 'register.html',context)
